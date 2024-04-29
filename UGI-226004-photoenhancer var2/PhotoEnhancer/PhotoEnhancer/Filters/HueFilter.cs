3e88a68886cfac27a8394dee464bc3c6c9fc878f
Ekaterina <katyaberdysheva@gmail.com>
2024-04-29 14:36:21 +0500

    задание 2 выполнено

---
+++ UGI-226004-photoenhancer var2/PhotoEnhancer/PhotoEnhancer/Filters/HueFilter.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PhotoEnhancer
+{
+    public class HueFilter : PixelFilter<HueParameters>
+    {
+        public override Pixel ProcessPixel(Pixel p, HueParameters parameters)
+        {
+            var q = Convertors.RGBToHSL(p);
+
+            var hue = q.H * 360 + parameters.Shift;
+
+            if (hue >= 360)
+                hue -= 360;
+
+            return Convertors.HSLToRGB(new PixelHSL(hue / 360, q.S, q.L));
+        } 
+
+        public override string ToString()
+        {
+            return "Оттенок";
+        }
+    }
+}
